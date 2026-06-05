7e9f2c592f1cd696acb6a3a33eeadbc4535f3d87
Rysya <pbl6a@bk.ru>
2026-06-05 12:48:37 +0300

    Initial Commit

---
+++ SmartConvert/SmartConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartConvertApp.swift
+//  SmartConvert
+//
+//  Created by Мария Александрова on 05.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct SmartConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
